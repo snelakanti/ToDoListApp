a7f219be78a8dbbc5e2349cf2a0a8722b66ed3db
snelakanti <vnelakanti@gmail.com>
2018-10-11 11:44:57 -0600

    create component todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AngularFireDatabaseModule } from "angularfire2/database";
 import { AppComponent } from "./app.component";
 import { environment } from "../environments/environment";
 import { initializeApp } from "firebase/app";
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
